849fe53de73961581bb75a8624496e99466891c9
Jibel <itsjibel@gmail.com>
2023-04-15 16:10:51 +0330

    Rename the test.cpp to linked_list_one_way.hpp

--- src/linked_list_one_way.hpp
+++ src/linked_list_one_way.hpp
@@ -165,16 +165,3 @@ class List
 	    }
 	}
 };
-
-int main()
-{
-    List myList;
-    myList.create_node(3);
-    myList.pop(6);
-    myList.append(9);
-    myList.insert(7, 2);
-    myList.pop_back();
-	myList.erase(0);
-    myList.show();
-    myList.free_list();
-}
