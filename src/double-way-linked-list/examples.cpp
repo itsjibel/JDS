7c8736a9e5e7d32950108c921c6ed3b449f8eb7b
Jibel <itsjibel@gmail.com>
2023-04-19 14:02:30 +0330

    Add insert(), pop() and at() function into DWLL library

--- src/double-way-linked-list/examples.cpp
+++ src/double-way-linked-list/examples.cpp
@@ -5,9 +5,10 @@ using std::cout;
 int main()
 {
     LL_DW myList({1, 2, 3, 4});
-    myList.pop(6);
-    myList.append(9);
+    myList.pop(0);
+    myList.append(5);
     myList.insert(7, 2);
+    
     for (int i=0; i<myList.size(); i++)
         cout<<myList.at(i)<<"\n";
 }
