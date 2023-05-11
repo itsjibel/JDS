47d5c195716b16df45322b795ec35a05e7de4b74
Jibel <itsjibel@gmail.com>
2023-05-12 00:36:33 +0330

    Add simple array functions

---
+++ src/Array/array.hpp
@@ -0,0 +1,64 @@
+int lsearch (int arr[], int size, int key)
+{
+    while (size != -1)
+        if (arr[size--] == key) return size + 1;
+    return -1;
+}
+
+int bsearch (int arr[], int low, int high, int key)
+{
+    if (low <= high)
+    {
+        int mid = (high + low) / 2;
+        if (arr[mid] == key) return mid;
+        if (key < arr[mid]) return bsearch(arr, low, mid - 1, key);
+        if (key > arr[mid]) return bsearch(arr, mid + 1, high, key);
+    }
+    return -1;
+}
+
+int ibsearch(int arr[], int low, int high, int key)
+{
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) return mid;
+        if (key < arr[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return -1;
+}
+
+int sdel(int arr[], int size, int key)
+{
+    int pos = bsearch(arr, 0, size - 1, key);
+    if (pos == -1)
+        return size;
+    
+    for (int i=pos; i<size - 1; i++)
+        arr[i] = arr[i + 1];
+
+    return size - 1;
+}
+
+int del(int arr[], int size, int key)
+{
+    int pos = lsearch(arr, size - 1, key);
+    if (pos == -1)
+        return size;
+    
+    for (int i=pos; i<size - 1; i++)
+        arr[i] = arr[i + 1];
+
+    return size - 1;
+}
+
+int insert(int arr[], int size, int index, int key)
+{
+    if (index >= size)
+        return -1;
+    arr[index] = key;
+    return index;
+}
---
+++ src/Array/examples.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "array.hpp"
+using std::cout;
+
+int main()
+{
+    int a[4] {1, 5, 8, 92};
+    cout<<lsearch(a, sizeof(a)/4, 5)<<'\n';
+    cout<<bsearch(a, 0, sizeof(a)/4, 92)<<'\n';
+    cout<<ibsearch(a, 0, sizeof(a)/4, 8)<<'\n';
+    sdel(a, sizeof(a)/4, 8);
+    cout<<ibsearch(a, 0, sizeof(a)/4, 8)<<'\n';
+    del(a, sizeof(a)/4, 5);
+    cout<<bsearch(a, 0, sizeof(a)/4, 5)<<'\n';
+
+    return 0;
+}
