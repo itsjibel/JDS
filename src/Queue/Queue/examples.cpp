8de6735a603ad4164b8d9c5c92a5647b2f3f1467
Jibel <itsjibel@gmail.com>
2023-06-08 13:39:55 +0330

    Add 'add()' and 'at()' function

--- src/Queue/Queue/examples.cpp
+++ src/Queue/Queue/examples.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
 #include "queue.hpp"
+#include <iostream>
 
 int main()
 {
-    Queue<int> b(5);
+    Queue<int> myQueue(5);
+    myQueue.add(1);
+    myQueue.add(2);
+    myQueue.add(3);
+    myQueue.add(4);
+    myQueue.add(5);
+    for (int i=0; i<myQueue.size(); i++)
+        std::cout<<myQueue.at(i)<<'\t';
     return 0;
 }
--- src/Queue/Queue/queue.hpp
+++ src/Queue/Queue/queue.hpp
@@ -1,18 +1,21 @@
 template <typename T> class Queue
 {
     private:
-        T **queue;
-        long long int front=-1;
-        long long int rear=-1;
-        long long int size=0;
+        T *queue;
+        long long int front;
+        long long int rear;
+        long long int _size;
 
     public:
         unsigned long long int capacity;
         Queue(unsigned long long int c) : capacity{c}
         {
-            queue = new T *[capacity];
+            queue = new T[capacity];
+            front=0;
+            rear=0;
+            _size=0;
         }
-        
+
         ~Queue()
         {
             delete[] queue;
@@ -20,11 +23,30 @@ template <typename T> class Queue
 
         bool is_empty() const
         {
-            return size == 0;
+            return _size == 0;
         }
 
         bool is_full() const
         {
-            return size == capacity - 1;
+            return _size == capacity;
+        }
+
+        bool add(T val)
+        {
+            if (is_full() || front == capacity)
+                return false;
+            queue[front++] = val;
+            _size++;
+            return true;
+        }
+
+        T at(unsigned long long int i) const
+        {
+            return queue[i];
+        }
+
+        unsigned long long int size() const
+        {
+            return capacity;
         }
 };
