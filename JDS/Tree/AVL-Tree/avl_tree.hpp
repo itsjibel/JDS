7d469d8e1f7d807cb07f245b5e62b868281470ce
Jibel <itsjibel@gmail.com>
2023-07-23 10:26:00 +0330

    Complete the AVL Tree

--- JDS/Tree/AVL-Tree/avl_tree.hpp
+++ JDS/Tree/AVL-Tree/avl_tree.hpp
@@ -19,4 +19,86 @@ public:
     {
         return new AVLTree(b);
     }
+
+    Node* rightRotate(Node* y)
+    {
+        Node *x, *T2;
+
+        x = y->left;
+        T2 = x->right;
+
+        x->right = y;
+        y->left = T2;
+
+        y->height = std::max(height(y->left), height(y->right)) + 1;
+        x->height = std::max(height(x->left), height(x->right)) + 1;
+
+        return x;
+    }
+
+    Node* leftRotate(Node* x)
+    {
+        Node *y, *T2;
+
+        y = x->right;
+        T2 = y->left;
+
+        y->left = x;
+        x->right = T2;
+
+        x->height = std::max(height(x->left), height(x->right)) + 1;
+        y->height = std::max(height(y->left), height(y->right)) + 1;
+
+        return y;
+    }
+
+    lli balance(Node* p)
+    {
+        if (p == NULL)
+            return 0;
+        return height(p->left) - height(p->right);
+    }
+
+    Node* add(Node* p, const lli data)
+    {
+        if (p == NULL)
+        {
+            Node* np = new Node;
+            np->data = data;
+            return np;
+        }
+
+        if (data < p->data)
+            p->left = add(p->left, data);
+        else if (data > p->data)
+            p->right = add(p->right, data);
+        else
+            return p;
+
+        p->height = 1 + std::max(height(p->left), height(p->right));
+
+        lli b = balance(p);
+
+        if (p->left != NULL)
+        if (b >= 2 && data < p->left->data)
+            return rightRotate(p);
+
+        if (p->right != NULL)
+        if (b >= -2 && data > p->right->data)
+            return leftRotate(p);
+
+        if (b >= 2 && data > p->left->data)
+        {
+            p->left = leftRotate(p->left);
+            return rightRotate(p);
+        }
+
+        if (b <= -2 && data < p->right->data)
+        {
+            p->right = rightRotate(p->right);
+            return leftRotate(p);
+        }
+
+        return p;
+    }
 };
--- examples/avl_tree.cpp
+++ examples/avl_tree.cpp
@@ -3,6 +3,16 @@
 int main()
 {
     AVLTree* myAVLTree = AVLTree::createInstance(9);
+    myAVLTree->add(myAVLTree->n, 56);
+    myAVLTree->add(myAVLTree->n, 7);
+    myAVLTree->add(myAVLTree->n, 23);
+    myAVLTree->add(myAVLTree->n, 3);
+    myAVLTree->add(myAVLTree->n, 5);
+    myAVLTree->add(myAVLTree->n, 18);
+    myAVLTree->add(myAVLTree->n, 8);
+
+    std::cout<<"Tree:"<<std::endl;
+    myAVLTree->draw_tree(myAVLTree->n);
 
     myAVLTree->deleteTree(myAVLTree->n);
     return 0;
