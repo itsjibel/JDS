48c3ef4993b6b5f68a6859550f17ec42e7c77147
Jibel <itsjibel@gmail.com>
2023-07-23 10:56:16 +0330

    Add add() function

--- JDS/Tree/Heap-Tree/Min-Heap/min_heap.hpp
+++ JDS/Tree/Heap-Tree/Min-Heap/min_heap.hpp
@@ -4,8 +4,29 @@ class MinHeap
 {
 private:
     typedef long long int lli;
-    std::vector<lli> min_heap_arr;
 
 public:
-;
+    std::vector<lli> min_heap_arr;
+
+    lli parent (lli i)
+    {
+        return (i-1) / 2;
+    }
+
+    void add(lli val)
+    {
+        lli i = min_heap_arr.size();
+        min_heap_arr.push_back(val);
+
+        while (i != 0 && min_heap_arr[i] < min_heap_arr[parent(i)])
+        {
+            std::swap(min_heap_arr[i], min_heap_arr[parent(i)]);
+            i = parent(i);
+        }
+    }
+
+    unsigned long int size() const
+    {
+        return min_heap_arr.size();
+    }
 };
--- examples/min_heap.cpp
+++ examples/min_heap.cpp
@@ -3,6 +3,17 @@
 
 int main()
 {
-    MinHeap a;
+    MinHeap myMinHeap;
+
+    myMinHeap.add(4);
+    myMinHeap.add(8);
+    myMinHeap.add(3);
+    myMinHeap.add(1);
+    myMinHeap.add(5);
+    myMinHeap.add(2);
+
+    for (long long int number : myMinHeap.min_heap_arr)
+        std::cout<<number<<std::endl;
+
     return 0;
 }
