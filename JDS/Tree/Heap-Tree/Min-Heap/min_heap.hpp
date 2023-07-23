ae01df4b0f51b00f410074492f2ea3da072f82ac
Jibel <itsjibel@gmail.com>
2023-07-23 10:42:53 +0330

    Start the Heap Tree section

---
+++ JDS/Tree/Heap-Tree/Min-Heap/min_heap.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class MinHeap
+{
+private:
+    typedef long long int lli;
+    std::vector<lli> min_heap_arr;
+
+public:
+;
+};
---
+++ examples/min_heap.cpp
@@ -0,0 +1,8 @@
+#include "../JDS/Tree/Heap-Tree/Min-Heap/min_heap.hpp"
+#include <iostream>
+
+int main()
+{
+    MinHeap a;
+    return 0;
+}
