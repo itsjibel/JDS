cd5ee2b93042ef116a3183eb1c65a9b03407ae1a
Jibel <itsjibel@gmail.com>
2023-08-07 10:50:46 +0330

    Complete bubble sort

--- JDS/Sort/bubble.hpp
+++ JDS/Sort/bubble.hpp
@@ -1,4 +1,9 @@
-long long int* bubbleSort(long long int arr[])
+#include <algorithm>
+
+void bubbleSort(long long int* arr, unsigned long int arrSize)
 {
-    return arr;
+    for (auto i{0}; i<arrSize - 1; i++)
+        for (auto j{0}; j<arrSize - 1; j++)
+            if (arr[j] > arr[j + 1])
+                std::swap(arr[j], arr[j + 1]);
 }
--- examples/bubble_sort.cpp
+++ examples/bubble_sort.cpp
@@ -1,6 +1,13 @@
 #include "../JDS/Sort/bubble.hpp"
+#include <iostream>
 
 int main()
 {
+    long long int array[10] {23, 43, 7, 5, -2, 13, 6, 10, 0, 21};
+    auto arraySize = sizeof(array) / sizeof(long long int);
+    bubbleSort(array, arraySize);
+
+    for (auto i{0}; i<arraySize; i++)
+        std::cout<<array[i]<<(i < arraySize - 1 ? ", " : "\n");
     return 0;
 }
