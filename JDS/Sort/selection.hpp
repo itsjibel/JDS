41b26613795eba8e91902504ade93215230c842f
Jibel <itsjibel@gmail.com>
2023-08-07 11:08:14 +0330

    Complete selection sort

--- JDS/Sort/selection.hpp
+++ JDS/Sort/selection.hpp
@@ -2,4 +2,15 @@
 
 void selectionSort(long long int* arr, unsigned long int arrSize)
 {
+    for (auto i{0}; i<arrSize - 1; i++)
+    {
+        long long int minValIndex = i;
+        for (auto j{i}; j<arrSize; j++)
+            if (arr[j] < arr[minValIndex])
+                minValIndex = j;
+
+        auto j = i;
+        while (arr[minValIndex] < arr[j])
+            std::swap(arr[minValIndex], arr[j--]);
+    }
 }
