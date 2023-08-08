c1d3619407ab8d50048c1a9918d634aaca44dd27
Jibel <itsjibel@gmail.com>
2023-08-08 11:44:06 +0330

    Complete quick sort

--- JDS/Sort/quick.hpp
+++ JDS/Sort/quick.hpp
@@ -1,8 +1,19 @@
 #include <algorithm>
 
 int partition(int* arr, const int start, const int end) {
-    return 0;
+    int i = start;
+    for (int j=start; j<end; j++)
+        if (arr[j] <= arr[end])
+            std::swap(arr[j], arr[i++]);
+
+    std::swap(arr[end], arr[i]);
+    return i;
 }
 
 void quickSort(int* arr, const int start, const int end) {
+    if (start < end) {
+        int q = partition(arr, start, end);
+        quickSort(arr, start, q - 1);
+        quickSort(arr, q + 1, end);
+    }
 }
