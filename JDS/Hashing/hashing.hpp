3b1169e2460ff0634911a2275e77b87cf0bec37e
Jibel <itsjibel@gmail.com>
2023-08-09 11:27:20 +0330

    Complete Separate Chaining

--- JDS/Hashing/hashing.hpp
+++ JDS/Hashing/hashing.hpp
@@ -1,3 +1,48 @@
-void separateChaining(const int arr[], int m)
-{
-}
+#include <iostream>
+
+struct Node {
+    int data {0};
+    Node* next;
+    Node(int data) : data(data) {}
+    Node();
+};
+
+class SeparateChaining {
+public:
+    int h(int key, int m)
+    {
+        return key % m;
+    }
+
+    Node** separateChaining(const int keys[], int n, int m)
+    {
+        Node** result = new Node*[m];
+
+        for (int i{0}; i<n; i++)
+        {
+            int hashResult = h(keys[i], m);
+            if (result[hashResult] != NULL)
+            {
+                Node* temp = result[hashResult]->next;
+                result[hashResult]->next = new Node(keys[i]);
+                result[hashResult]->next->next = temp;
+            } else
+                result[hashResult] = new Node(keys[i]);
+        }
+        return result;
+    }
+
+    void search(int key, int m, Node** result)
+    {
+        int hashResult = h(key, m);
+        int index = 0;
+        Node* itter = result[hashResult];
+        while (itter->data != key)
+            itter = itter->next, index++;
+
+        if (itter->data == key)
+            std::cout<<"'"<<key<<"' Found at chain "<<hashResult+1<<" and index "<<index<<std::endl;
+        else
+            std::cout<<"Not found in any chain"<<std::endl;
+    }
+};
--- examples/hashing.cpp
+++ examples/hashing.cpp
@@ -1,9 +1,18 @@
 #include "../JDS/Hashing/hashing.hpp"
-#include <iostream>
 
 int main()
 {
-    int array[10] {12, 7, 2, 43, 21, 8, 2, 3, 90, 2};
-    separateChaining(array, 5);
+    int m = 5;
+    int keys[10] {12, 7, 2, 43, 21, 8, 5, 3, 90, 10};
+    SeparateChaining mySeparateChaining;
+    Node** result = mySeparateChaining.separateChaining(keys, 10, m);
+
+    for (int i=0; i<m-1; i++)
+        for (Node* itter = result[i]; itter != NULL; itter = itter->next)
+            std::cout<<itter->data<<(itter->next != NULL ? " -> " : "\n");
+
+    mySeparateChaining.search(8, m, result);
+
+    delete[] result;
     return 0;
 }
